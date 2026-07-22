877fa125de8ea9818bc7428094292c940a3b339a
Ansh <anshgupta410@gmail.com>
2026-07-23 00:14:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    with open(file_path) as file:
+        requirements = [req.strip() for req in file.readlines()]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Ansh",
+    author_email="anshgupta410g@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
